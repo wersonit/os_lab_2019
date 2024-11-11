22b6e5fcad2d4c3cf672a36ece1e722c48a3f97c
user <user@localhost.localdomain>
2024-11-12 02:10:04 +0300

    lab4 commit, code fix

---
+++ lab4/src/min_max/build/CMakeFiles/parallel_min_max.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/projects/os-lab-2019/lab4/src/min_max/find_min_max.c" "CMakeFiles/parallel_min_max.dir/find_min_max.c.o" "gcc" "CMakeFiles/parallel_min_max.dir/find_min_max.c.o.d"
+  "/projects/os-lab-2019/lab4/src/min_max/parallel_min_max.c" "CMakeFiles/parallel_min_max.dir/parallel_min_max.c.o" "gcc" "CMakeFiles/parallel_min_max.dir/parallel_min_max.c.o.d"
+  "/projects/os-lab-2019/lab4/src/min_max/utils.c" "CMakeFiles/parallel_min_max.dir/utils.c.o" "gcc" "CMakeFiles/parallel_min_max.dir/utils.c.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ lab4/src/min_max/build/CMakeFiles/parallel_min_max.dir/find_min_max.c.o.d
@@ -0,0 +1,13 @@
+CMakeFiles/parallel_min_max.dir/find_min_max.c.o: \
+ /projects/os-lab-2019/lab4/src/min_max/find_min_max.c \
+ /usr/include/stdc-predef.h \
+ /projects/os-lab-2019/lab4/src/min_max/find_min_max.h \
+ /projects/os-lab-2019/lab4/src/min_max/utils.h \
+ /usr/lib/gcc/x86_64-redhat-linux/8/include/limits.h \
+ /usr/lib/gcc/x86_64-redhat-linux/8/include/syslimits.h \
+ /usr/include/limits.h /usr/include/bits/libc-header-start.h \
+ /usr/include/features.h /usr/include/sys/cdefs.h \
+ /usr/include/bits/wordsize.h /usr/include/bits/long-double.h \
+ /usr/include/gnu/stubs.h /usr/include/gnu/stubs-64.h \
+ /usr/include/bits/posix1_lim.h /usr/include/bits/local_lim.h \
+ /usr/include/linux/limits.h /usr/include/bits/posix2_lim.h
